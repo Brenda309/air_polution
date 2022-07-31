56200b520a6bb37bcdf433045348861c5efbfa7e
Brenda309 <brewihogora23@gmail.com>
2022-07-31 06:26:48 +0200

    Update the style

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/app';
 import store from './Redux/configureStore';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
